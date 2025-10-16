7bff86eea3278a93b6491f482b84296ed5038c9a
devruhul <ruhulamiin262001@gmail.com>
2025-10-16 01:53:27 +0100

    updated the comment

--- tuples.py
+++ tuples.py
@@ -1,9 +1,12 @@
-
+# tuples lists
 names = ('Alice', 'Bob', 'Charlie', 'Diana', 'Eve', 'Bob')
          
 print('original',names)
+# it will print from staring index not the last index
 print(names[1:-2])
 
 print(names.index('Charlie'))
+# find the 4th element but index 3
 print(names[3])
+# find how many times Bob is in the tuple
 print(names.count('Bob'))
